c398594b1c7041dfdd5f6277568494c4df0336e5
Sam Westfield <sam.westfield@introhive.com>
2017-10-27 11:47:50 -0300

    Added Drawer

--- src/App.js
+++ src/App.js
@@ -2,8 +2,19 @@ import React, { Component } from 'react';
 import logo from './SWElogo.png';
 import './App.css';
 import AppBar from 'material-ui/AppBar';
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
 
 class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {open: false};
+  }
+
+  handleToggle = () => this.setState({open: !this.state.open});
+
+  handleClose = () => this.setState({open:false});
 
   render() {
     return (
@@ -11,7 +22,22 @@ class App extends Component {
       <div className="App">
         <AppBar
           title="SWEDashboard"
+          onLeftIconButtonTouchTap={this.handleToggle}
         />
+        <Drawer
+          docked={false}
+          width={300}
+          open={this.state.open}
+          onRequestChange={(open) => this.setState({open})}
+        >
+          <AppBar
+            title="Menu"
+            onLeftIconButtonTouchTap={this.handleToggle}
+          />
+          <MenuItem onClick={this.handleClose}>Dashboard</MenuItem>
+          <MenuItem onClick={this.handleClose}>Resources</MenuItem>
+        </Drawer>
+
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to the SWE dashboard!</h1>
