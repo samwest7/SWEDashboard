6ef7e5fa9c414934c43e5d14114b1cc750b9ab10
Sam Westfield <sam.westfield@introhive.com>
2017-10-27 11:17:30 -0300

    Added Material UI AppBar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import logo from './SWElogo.png';
 import './App.css';
+import AppBar from 'material-ui/AppBar';
 
 class App extends Component {
+
   render() {
     return (
+      // <MuiThemeProvider muiTheme={getMuiTheme(lightBaseTheme)}>
       <div className="App">
+        <AppBar
+          title="SWEDashboard"
+        />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to the SWE dashboard!</h1>
--- src/index.js
+++ src/index.js
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const ThemedApp = () => (
+  <MuiThemeProvider muiTheme={getMuiTheme()}>
+    <App />
+  </MuiThemeProvider>
+)
+
+ReactDOM.render(<ThemedApp />, document.getElementById('root'));
 registerServiceWorker();
